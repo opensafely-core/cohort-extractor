e4821b15bc53d91d601eb052947d839fdcd55645
David Evans <dave@evans.io>
2020-04-02 17:03:16 +0100

    (Sort of) working MVP
    This is working in the sense that it successfully produces output but
    it's almost certainly not actually correct.

---
+++ datalab_cohorts/__init__.py
@@ -0,0 +1,279 @@
+import datetime
+import os
+import re
+from urllib.parse import urlparse, unquote
+
+import pandas
+import pyodbc
+
+
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "mssql://SA:ahsjdkaJAMSHDA123%5B@covid.ebmdatalab.net:1433/OPENCorona",
+)
+
+
+# Characters that are safe to interpolate into SQL (see
+# `placeholders_and_params` below)
+SAFE_CHARS_RE = re.compile(r"[a-zA-Z0-9_\.\-]+")
+
+
+class StudyDefinition:
+
+    population = None
+    _demographic_df = None
+    _chess_df = None
+    _db_connection = None
+
+    def __init__(self, population, **kwargs):
+        self.population_definition = population
+        self.covariate_definitions = kwargs
+
+    def to_csv(self, filename):
+        self.covariates = {}
+        self.population = self.run_query(*self.population_definition)
+        for name, (query_type, query_args) in self.covariate_definitions.items():
+            self.covariates[name] = self.run_query(query_type, query_args)
+        study_df = self.population
+        for column_name, df in self.covariates.items():
+            # Boolean covariates are just lists of patient IDs, we treat them
+            # as having an implicit 1 column
+            if len(df.columns) == 0:
+                df.insert(0, column_name, 1)
+            # Otherwise we rename the value column to match our output name
+            else:
+                df.rename({df.columns[0]: column_name}, axis=1, inplace=True)
+            study_df = study_df.merge(
+                df, how="left", left_index=True, right_index=True, copy=False
+            )
+        study_df.fillna(0, inplace=True)
+        study_df.to_csv(filename, index_label="patient_id")
+
+    def run_query(self, query_type, query_args):
+        method_name = f"patients_{query_type}"
+        method = getattr(self, method_name)
+        return method(**query_args)
+
+    def patients_age_as_of(self, reference_date):
+        demographic_df = self.get_demographic_df()
+        demographic_df["age"] = (reference_date - demographic_df["birth_date"]).astype(
+            "<m8[Y]"
+        )
+        return demographic_df[["age"]]
+
+    def patients_sex(self):
+        demographic_df = self.get_demographic_df()
+        return demographic_df[["sex"]]
+
+    def get_demographic_df(self):
+        if self._demographic_df is not None:
+            return self._demographic_df
+        if self.population is None:
+            where_condition = ""
+            params = []
+        else:
+            placeholders, params = placeholders_and_params(self.population.index)
+            where_condition = f"WHERE patient_id IN ({placeholders})"
+        self._demographic_df = self.sql_to_df(
+            f"""
+            SELECT
+              Patient_ID AS patient_id,
+              DateOfBirth AS birth_date,
+              Sex as sex
+            FROM Patient
+            {where_condition}
+            """,
+            params,
+        )
+        return self._demographic_df
+
+    def patients_with_these_medications(self, codelist, min_date=None, max_date=None):
+        """
+        Patients who have been prescribed at least one of this list of
+        medications in the defined period
+        """
+        placeholders, params = placeholders_and_params(codelist)
+        date_condition, date_params = make_date_filter(
+            "ConsultationDate", min_date, max_date
+        )
+        params.extend(date_params)
+        return self.sql_to_df(
+            f"""
+            SELECT DISTINCT med.Patient_ID AS patient_id
+            FROM MedicationDictionary AS dict
+            INNER JOIN MedicationIssue AS med
+            ON dict.MultilexDrug_ID = med.MultilexDrug_ID
+            WHERE dict.DMD_ID IN ({placeholders}) AND {date_condition}
+            ORDER BY patient_id
+            """,
+            params,
+        )
+
+    def patients_with_these_clinical_events(
+        self, codelist, min_date=None, max_date=None
+    ):
+        """
+        Patients who have had at least one of these clinical events in the
+        defined period
+        """
+        placeholders, params = placeholders_and_params(codelist)
+        date_condition, date_params = make_date_filter(
+            "ConsultationDate", min_date, max_date
+        )
+        params.extend(date_params)
+        return self.sql_to_df(
+            f"""
+            SELECT DISTINCT Patient_ID AS patient_id
+            FROM CodedEvent
+            WHERE CTV3Code IN ({placeholders}) AND {date_condition}
+            ORDER BY patient_id
+            """,
+            params,
+        )
+
+    def patients_with_positive_covid_test(self):
+        chess_df = self.get_chess_df()
+        return chess_df[chess_df["result"] == "COVID-19"][[]]
+
+    def patients_have_died(self):
+        chess_df = self.get_chess_df()
+        return chess_df[chess_df["died"] == 1][[]]
+
+    def patients_admitted_to_itu(self):
+        chess_df = self.get_chess_df()
+        return chess_df[chess_df["admitted_itu"] == 1][[]]
+
+    def get_chess_df(self):
+        if self._chess_df is not None:
+            return self._chess_df
+        self._chess_df = pandas.read_csv("mvp_data/dummy_chess.csv")
+        return self._chess_df
+
+    def sql_to_df(self, sql, params):
+        return pandas.read_sql_query(
+            sql, self.get_db_connection(), params=params, index_col="patient_id"
+        )
+
+    def get_db_connection(self):
+        if self._db_connection:
+            return self._db_connection
+        parsed = urlparse(DATABASE_URL)
+        hostname = parsed.hostname
+        port = parsed.port or 1433
+        database = parsed.path.lstrip("/")
+        username = unquote(parsed.username)
+        password = unquote(parsed.password)
+        connection_str = (
+            f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+            f"SERVER={hostname},{port};"
+            f"DATABASE={database};"
+            f"UID={username};"
+            f"PWD={password}"
+        )
+        self._db_connection = pyodbc.connect(connection_str)
+        return self._db_connection
+
+
+class patients:
+    """
+    This will be a module eventually but a class with a bunch of static methods
+    is an easy way of making a namespace for now.
+
+    These methods don't *do* anything apart from some basic error checking;
+    they just return their name and arguments. This provides a friendlier API
+    then having to build some big nested data structure by hand and means we
+    can make use of autocomplete, docstrings etc to make it a bit more
+    discoverable.
+    """
+
+    @staticmethod
+    def age_as_of(reference_date):
+        if reference_date == "today":
+            reference_date = datetime.date.today()
+        else:
+            reference_date = datetime.date.fromisoformat(str(reference_date))
+        return "age_as_of", locals()
+
+    @staticmethod
+    def sex():
+        return "sex", locals()
+
+    @staticmethod
+    def with_these_medications(codelist, min_date=None, max_date=None):
+        assert codelist.system == "snomed"
+        return "with_these_medications", locals()
+
+    @staticmethod
+    def with_these_clinical_events(codelist, min_date=None, max_date=None):
+        assert codelist.system == "ctv3"
+        return "with_these_clinical_events", locals()
+
+    # The below are placeholder methods we don't expect to make it into the final API.
+    # They use a handler which returns dummy CHESS data.
+
+    @staticmethod
+    def with_positive_covid_test():
+        return "with_positive_covid_test", locals()
+
+    @staticmethod
+    def have_died():
+        return "have_died", locals()
+
+    @staticmethod
+    def admitted_to_itu():
+        return "admitted_to_itu", locals()
+
+
+def placeholders_and_params(values, as_ints=False):
+    """
+    Returns parameter placeholders for use in an SQL `IN` condition, together
+    with a list of parameters to be used.
+
+    Ideally the function would just be this:
+
+      placeholders = ','.join("?" * len(values))
+      params = values
+
+    However, the pyodbc driver uses "prepared statements" under the hood and
+    these have a maximum limit of 2100 parameters, which we exceed when using
+    large codelists. (See: https://github.com/mkleehammer/pyodbc/issues/576).
+    One way of working around this would be to use temporary tables, but for
+    now we just manually interpolate the values into the SQL. Rather than
+    attempt any escaping we simply apply a whitelist of known safe characters.
+    As the codes we use come from quite a restricted character set this
+    shouldn't be a problem. And if it is we'll just blow up with an error
+    rather than do anything dangerous.
+    """
+    if as_ints:
+        raise NotImplementedError("TODO")
+    values = list(map(str, values))
+    for value in values:
+        if not SAFE_CHARS_RE.match(value):
+            raise ValueError(f"Value contains disallowed characters: {value}")
+    quoted_values = [f"'{value}'" for value in values]
+    placeholders = ",".join(quoted_values)
+    params = []
+    return placeholders, params
+
+
+def make_date_filter(column, min_date, max_date):
+    if min_date is not None and max_date is not None:
+        return f"{column} BETWEEN ? AND ?", [min_date, max_date]
+    elif min_date is not None:
+        return f"{column} >= ", [min_date]
+    elif max_date is not None:
+        return f"{column} <= ", [max_date]
+    else:
+        return "1=1", []
+
+
+# Quick and dirty hack until we have a proper library for codelists
+class Codelist(list):
+    system = None
+
+
+def codelist_from_csv(filename, system):
+    codes = pandas.read_csv(filename)["code"]
+    codes = Codelist(codes)
+    codes.system = system
+    return codes
--- generate_cohort.py
+++ generate_cohort.py
@@ -1,68 +1,11 @@
-"""This module should be replaced by the thing Dave is writing which
-will generate data from a series of function calls
-
-
-
-"""
-from contextlib import contextmanager
-import requests
 import os
-import pyodbc
-import datetime
-import pandas as pd
-
-
-@contextmanager
-def closing_connection():
-    server = f"{os.environ['DB_HOST']},1433"
-    database = os.environ["DB_NAME"]
-    username = "SA"
-    password = os.environ["DB_PASS"]
-    dsn = (
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + server
-        + ";DATABASE="
-        + database
-        + ";UID="
-        + username
-        + ";PWD="
-        + password
-    )
-    cnxn = pyodbc.connect(dsn)
-    yield cnxn
-    cnxn.close()
-
-
-def sql_to_df(sql, params=[]):
-    with closing_connection() as cnxn:
-        df = pd.read_sql(sql, cnxn, params=params)
-    return df
-
-
-def patients_with_age_condition(direction, age):
-    allowed_directions = [">", "<", ">=", "<="]
-    assert (
-        direction in allowed_directions
-    ), f"direction must be one of {allowed_directions}"
-    today = datetime.date.today().strftime("%Y-%m-%d")
-    return sql_to_df(
-        f"""
-        SELECT
-          Patient_ID, DATEDIFF(year, DateOfBirth, ?) AS age
-        FROM
-          Patient
-        WHERE
-          DATEDIFF(year, DateOfBirth, ?) {direction} ?""",
-        params=[today, today, age],
-    )
-
-
-def main():
-    df = patients_with_age_condition("<", 70)
-    target_path = "analysis/input.csv"
-    df.to_csv(target_path)
-    print(f"Data written to {target_path}")
+import sys
 
 
 if __name__ == "__main__":
-    main()
+    this_dir = os.path.dirname(__file__)
+    sys.path.extend([this_dir, os.path.join(this_dir, 'analysis')])
+    # Avoid creating __pycache__ files in the analysis directory
+    sys.dont_write_bytecode = True
+    from study_definition import study
+    study.to_csv('analysis/input.csv')
