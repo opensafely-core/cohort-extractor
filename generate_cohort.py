a3aeea027c45bfda4536e3482c407b0ee716ae21
Seb Bacon <seb.bacon@gmail.com>
2020-04-02 13:40:55 +0100

    Make our cohort generation stub do pyodbc things
    This should be a decent test of if pyinstaller works

--- generate_cohort.py
+++ generate_cohort.py
@@ -4,18 +4,63 @@ will generate data from a series of function calls
 
 
 """
-
+from contextlib import contextmanager
 import requests
+import os
+import pyodbc
+import datetime
+import pandas as pd
+
+
+@contextmanager
+def closing_connection():
+    server = f"{os.environ['DB_HOST']},1433"
+    database = os.environ["DB_NAME"]
+    username = "SA"
+    password = os.environ["DB_PASS"]
+    dsn = (
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + server
+        + ";DATABASE="
+        + database
+        + ";UID="
+        + username
+        + ";PWD="
+        + password
+    )
+    cnxn = pyodbc.connect(dsn)
+    yield cnxn
+    cnxn.close()
+
+
+def sql_to_df(sql, params=[]):
+    with closing_connection() as cnxn:
+        df = pd.read_sql(sql, cnxn, params=params)
+    return df
+
+
+def patients_with_age_condition(direction, age):
+    allowed_directions = [">", "<", ">=", "<="]
+    assert (
+        direction in allowed_directions
+    ), f"direction must be one of {allowed_directions}"
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    return sql_to_df(
+        f"""
+        SELECT
+          Patient_ID, DATEDIFF(year, DateOfBirth, ?) AS age
+        FROM
+          Patient
+        WHERE
+          DATEDIFF(year, DateOfBirth, ?) {direction} ?""",
+        params=[today, today, age],
+    )
 
 
 def main():
-    response = requests.get(
-        "https://raw.githubusercontent.com/ebmdatalab/tpp-sql-notebook/master/data/analysis/final_dataset.csv?token=AABTSR3LGB2FPTIWF4CMMH26RXFPE"
-    )
-    response.raise_for_status()
+    df = patients_with_age_condition("<", 70)
     target_path = "analysis/input.csv"
-    with open(target_path, "w") as f:
-        f.write(response.text)
+    df.to_csv(target_path)
     print(f"Data written to {target_path}")
 
 
