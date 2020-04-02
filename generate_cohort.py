3e53ed992e48f61d639344b0d767086ee5d37b3d
Seb Bacon <seb.bacon@gmail.com>
2020-04-02 12:11:21 +0100

    Don't run the data generation within docker (it's unecessary)

--- generate_cohort.py
+++ generate_cohort.py
@@ -8,14 +8,16 @@ will generate data from a series of function calls
 import requests
 
 
-def get_data():
+def main():
     response = requests.get(
         "https://raw.githubusercontent.com/ebmdatalab/tpp-sql-notebook/master/data/analysis/final_dataset.csv?token=AABTSR3LGB2FPTIWF4CMMH26RXFPE"
     )
     response.raise_for_status()
-    with open("analysis/input.csv", "w") as f:
+    target_path = "analysis/input.csv"
+    with open(target_path, "w") as f:
         f.write(response.text)
+    print(f"Data written to {target_path}")
 
 
 if __name__ == "__main__":
-    get_data()
+    main()
--- run_model.py
+++ run_model.py
@@ -4,11 +4,23 @@ import subprocess
 
 
 def check_output():
+    # Stata insists on writing to the current
+    # directory: https://stackoverflow.com/a/35051922/559140
     with open("model.log", "r") as f:
         output = f.read()
         print(output)
         if re.match(r"^r\([0-9]+\);$", output):
             raise Exception("Problem found")
+
+
+def fix_permissions():
+    """Ensure generated files are world-writable.
+
+    Ugly hack until we get round to changing the base docker image to
+    run as a user other than root.
+
+    """
+    os.chmod("model.log", 0o666)
 
 
 def clear_output():
@@ -32,4 +44,5 @@ def run_model():
 if __name__ == "__main__":
     clear_output()
     run_model()
+    fix_permissions()
     check_output()
