b77b9013d3e78d41562f4d6b4babffc1e4aee177
Seb Bacon <seb.bacon@gmail.com>
2020-04-06 11:53:15 +0100

    Provide a GUI for Windows users

--- run.py
+++ run.py
@@ -1,21 +1,40 @@
-"""A cross-platform script to build and start a notebook, open a web
-browser on the correct port, and handle shutdowns gracefully
-
+"""A cross-platform script to build cohorts, run models, build and
+start a notebook, open a web browser on the correct port, and handle
+shutdowns gracefully
 """
+
 import os
 import re
 import subprocess
 import socket
 import time
 import urllib.request
-import argparse
 
-import generate_cohort
 
+
+try:
+    from gooey import Gooey
+    from gooey import GooeyParser as ArgumentParser
+
+    GOOEY_INSTALLED = True  # Currently, only in a Windows build
+except ImportError:
+    GOOEY_INSTALLED = False
+    from argparse import ArgumentParser
 
 tag = "docker.pkg.github.com/ebmdatalab/stata-docker-runner/stata-mp:latest"
-current_dir = os.getcwd()
 target_dir = "/home/app/notebook"
+
+
+def relative_dir():
+    if sys.executable.endswith(".exe"):
+        # This is a pyinstaller package on Windows; the `cwd` is
+        # likely to be anything (e.g. the Github desktop AppData
+        # space); `__file__` is some kind of temporary location
+        # resulting from an internal unzip.
+        relative_dir = os.path.dirname(os.path.realpath(sys.executable))
+    else:
+        relative_dir = os.path.dirname(os.path.realpath(__file__))
+    return relative_dir
 
 
 def await_jupyter_http(port):
@@ -96,6 +115,7 @@ def docker_run(tag, *args):
     again
 
     """
+    current_dir = relative_dir()
     runcmd = [
         "docker",
         "run",
@@ -146,33 +166,83 @@ def check_output():
     return output
 
 
-def clear_output():
-    try:
-        print("Removing existing output")
-        os.remove("model.log")
-    except FileNotFoundError:
-        pass
+def generate_cohort():
+    sys.path.extend([relative_dir(), os.path.join(relative_dir(), "analysis")])
+    # Avoid creating __pycache__ files in the analysis directory
+    sys.dont_write_bytecode = True
+    from study_definition import study
+
+    study.to_csv("analysis/input.csv")
+    print("Successfully created cohort and covariates at analysis/input.csv")
 
 
-def run_model(folder):
-    docker_run(tag, "-b", "do", f"{folder}/model.do")
+def run_model(folder, stata_path=None):
+    # XXX it's /e on windows
+    # XXX and windows doesn't know where we are now
+    args = ["-b", "do", f"{folder}/model.do"]
+    if not stata_path:
+        docker_run(tag, *args)
+    else:
+        stream_subprocess_output([stata_path] + args)
     return check_output()
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run models")
-    parser.add_argument("command", choices=["run", "generate_cohort", "test", "shell"])
-    parser.add_argument(
-        "--skip-build", help="Skip docker image build step", action="store_true"
+def main():
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(help="sub-command help")
+    generate_cohort_parser = subparsers.add_parser(
+        "generate_cohort", help="Generate cohort"
     )
-    args = parser.parse_args()
-    docker_login()
-    docker_pull()
-    if args.command == "shell":
-        docker_build_and_run("/bin/bash", skip_build=args.skip_build)
-    if args.command == "run":
-        print(run_model("analysis"))
-    if args.command == "test":
-        run_model("tests")
-    elif args.command == "generate_cohort":
-        generate_cohort.main()
+    generate_cohort_parser.set_defaults(which="generate_cohort")
+    run_model_parser = subparsers.add_parser("run", help="Run model")
+    run_model_parser.set_defaults(which="run")
+
+    generate_cohort_parser.add_argument(
+        "--database-url",
+        help="Database URL to query",
+        type=str,
+        required=True,
+        default=os.environ.get("DATABASE_URL", ""),
+    )
+    if GOOEY_INSTALLED:
+        run_model_parser.add_argument(
+            "--stata-path",
+            help="Path to Stata executable. Will use docker version if left empty",
+            widget="FileChooser",
+        )
+    else:
+        run_model_parser.add_argument(
+            "--stata-path",
+            help="Path to Stata executable. Will use docker version if left empty",
+        )
+    run_target_group = run_model_parser.add_mutually_exclusive_group()
+    run_target_group.add_argument(
+        "--analysis",
+        action="store_true",
+        help="Run stata against model at analysis/model.do",
+    )
+    run_target_group.add_argument(
+        "--test", action="store_true", help="Run stata using internal test model"
+    )
+
+    options = parser.parse_args()
+
+    if options.which == "run":
+        if options.test:
+            try:
+                run_model("tests", options.stata_path)
+                print("SUCCESS running tests")
+            except subprocess.CalledProcessError:
+                print("ERROR running tests")
+                raise
+        else:
+            print(run_model("analysis", options.stata_path))
+    elif options.which == "generate_cohort":
+        generate_cohort()
+
+
+if __name__ == "__main__":
+    if GOOEY_INSTALLED:
+        Gooey(main)()
+    else:
+        main()
