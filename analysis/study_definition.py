3880e19eb1f0e4540387d34de082ab2590abe04c
Seb Bacon <seb.bacon@gmail.com>
2020-04-06 11:44:43 +0100

    Add a minimally-useful study definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -5,18 +5,17 @@ cvd_meds = codelist_from_csv("mvp_data/cvd_medcodes.csv", system="snomed")
 chd_codes = codelist_from_csv("mvp_data/chd_codes.csv", system="ctv3")
 smoking_codes = codelist_from_csv("mvp_data/smoking_codes.csv", system="ctv3")
 
-
 study = StudyDefinition(
     # This line defines the study population
-    population=patients.with_positive_covid_test(),
+    population=patients.all(),
     # The rest of the lines define the covariates
     age=patients.age_as_of("today"),
     sex=patients.sex(),
+    BMI=patients.bmi("today"),
     cdv_meds=patients.with_these_medications(cvd_meds),
     chd_code=patients.with_these_clinical_events(chd_codes),
     smoking_status=patients.with_these_clinical_events(
         smoking_codes, min_date="2015-01-01", max_date="2020-03-31"
     ),
-    died=patients.have_died(),
-    admitted_itu=patients.admitted_to_itu(),
+    died=patients.random_sample(percent=5),
 )
