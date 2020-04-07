ec66f2d37152030976b09985a6649f8794f6684d
Seb Bacon <seb.bacon@gmail.com>
2020-04-07 13:14:32 +0100

    Run tests using native github actions container support

--- generate_cohort.py
+++ generate_cohort.py
@@ -2,10 +2,15 @@ import os
 import sys
 
 
-if __name__ == "__main__":
+def main():
     this_dir = os.path.dirname(__file__)
-    sys.path.extend([this_dir, os.path.join(this_dir, 'analysis')])
+    sys.path.extend([this_dir, os.path.join(this_dir, "analysis")])
     # Avoid creating __pycache__ files in the analysis directory
     sys.dont_write_bytecode = True
     from study_definition import study
-    study.to_csv('analysis/input.csv')
+
+    study.to_csv("analysis/input.csv")
+
+
+if __name__ == "__main__":
+    main()
