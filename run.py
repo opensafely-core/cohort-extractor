722dbbcd83b0751cbf18f573646101733945ba16
Seb Bacon <seb.bacon@gmail.com>
2020-04-09 12:33:03 +0100

    Add support for jupyter notebooks

--- run.py
+++ run.py
@@ -6,10 +6,11 @@ shutdowns gracefully
 import os
 import re
 import subprocess
+import signal
 import socket
 import time
 import urllib.request
-
+import webbrowser
 
 
 try:
@@ -21,7 +22,8 @@ except ImportError:
     GOOEY_INSTALLED = False
     from argparse import ArgumentParser
 
-tag = "docker.pkg.github.com/ebmdatalab/stata-docker-runner/stata-mp:latest"
+stata_image = "docker.pkg.github.com/ebmdatalab/stata-docker-runner/stata-mp:latest"
+notebook_tag = "opencorona-research"
 target_dir = "/home/app/notebook"
 
 
@@ -81,36 +83,35 @@ def stream_subprocess_output(cmd):
 def docker_build(tag):
     """Build container for Dockerfile in current directory
     """
+    buildcmd = ["docker", "build", "-t", tag, "-f", "Dockerfile", "."]
     print(
         "Building docker image. This may take some time (particularly on the first run)..."
     )
-    buildcmd = ["docker", "build", "-t", tag, "-f", "Dockerfile", "."]
+    print("Running", " ".join(buildcmd))
     stream_subprocess_output(buildcmd)
 
 
 def docker_login():
     runcmd = [
-        "echo",
-        os.environ["GITHUB_TOKEN"],
-        "|",
         "docker",
         "login",
         "docker.pkg.github.com",
         "-u",
         os.environ["GITHUB_ACTOR"],
-        "--password-stdin",
+        "--password",
+        os.environ["GITHUB_TOKEN"],
     ]
     print("Running {}".format(" ".join(runcmd)))
     return stream_subprocess_output(runcmd)
 
 
-def docker_pull():
-    runcmd = ["docker", "pull", tag]
+def docker_pull(image):
+    runcmd = ["docker", "pull", image]
     print("Running {}".format(" ".join(runcmd)))
     return stream_subprocess_output(runcmd)
 
 
-def docker_run(tag, *args):
+def docker_run(image, *args, detach=False):
     """Run docker in background, and install signal handler to stop it
     again
 
@@ -119,19 +120,34 @@ def docker_run(tag, *args):
     runcmd = [
         "docker",
         "run",
+        f"--detach={detach and 'true' or 'false'}",
         "-w",
         target_dir,
         "--rm",  # clean up the container after it's stopped
         "--mount",
         f"source={current_dir},dst={target_dir},type=bind",
+        "--publish-all",
         "--mount",
         # Ensure we override any local pyenv configuration
         f"source={current_dir}/.python-version-docker,dst={target_dir}/.python-version,type=bind",
-        tag,
+        image,
         *args,
     ]
     print("Running {}".format(" ".join(runcmd)))
-    return stream_subprocess_output(runcmd)
+    if detach:
+        completed_process = subprocess.run(runcmd, check=True, capture_output=True)
+        container_id = completed_process.stdout.decode("utf8").strip()
+
+        def stop_handler(sig, frame):
+            print("Stopping docker...")
+            subprocess.run(["docker", "kill", container_id], check=True)
+            sys.exit(0)
+
+        signal.signal(signal.SIGINT, stop_handler)
+
+        return container_id
+    else:
+        return stream_subprocess_output(runcmd)
 
 
 def docker_port(container_id):
@@ -147,8 +163,8 @@ def docker_port(container_id):
 
 def docker_build_and_run(*args, skip_build=False):
     if not skip_build:
-        docker_build(tag)
-    result = docker_run(tag, *args)
+        docker_build(image)
+    result = docker_run(image, *args)
     print(result)
 
 
@@ -181,7 +197,9 @@ def run_model(folder, stata_path=None):
     # XXX and windows doesn't know where we are now
     args = ["-b", "do", f"{folder}/model.do"]
     if not stata_path:
-        docker_run(tag, *args)
+        # XXX they'll need to log in docker_login()? Ensure
+        # environment variables are set
+        docker_run(stata_image, *args)
     else:
         stream_subprocess_output([stata_path] + args)
     return check_output()
@@ -196,7 +214,10 @@ def main():
     generate_cohort_parser.set_defaults(which="generate_cohort")
     run_model_parser = subparsers.add_parser("run", help="Run model")
     run_model_parser.set_defaults(which="run")
+    run_notebook_parser = subparsers.add_parser("notebook", help="Run notebook")
+    run_notebook_parser.set_defaults(which="notebook")
 
+    # Cohort parser options
     generate_cohort_parser.add_argument(
         "--database-url",
         help="Database URL to query",
@@ -204,17 +225,16 @@ def main():
         required=True,
         default=os.environ.get("DATABASE_URL", ""),
     )
+
+    # Model runner options
+    run_model_stata_kwargs = {
+        "help": "Path to Stata executable. Will use docker version if left empty"
+    }
     if GOOEY_INSTALLED:
-        run_model_parser.add_argument(
-            "--stata-path",
-            help="Path to Stata executable. Will use docker version if left empty",
-            widget="FileChooser",
-        )
-    else:
-        run_model_parser.add_argument(
-            "--stata-path",
-            help="Path to Stata executable. Will use docker version if left empty",
-        )
+        run_model_stata_kwargs["widget"] = "FileChooser"
+
+    run_model_parser.add_argument("--stata-path", **run_model_stata_kwargs)
+
     run_target_group = run_model_parser.add_mutually_exclusive_group()
     run_target_group.add_argument(
         "--analysis",
@@ -225,8 +245,12 @@ def main():
         "--test", action="store_true", help="Run stata using internal test model"
     )
 
-    options = parser.parse_args()
+    # Notebook runner options at the moment
+    run_notebook_parser.add_argument(
+        "--skip-build", action="store_true", help="Skip docker build step"
+    )
 
+    options = parser.parse_args()
     if options.which == "run":
         if options.test:
             try:
@@ -239,6 +263,17 @@ def main():
             print(run_model("analysis", options.stata_path))
     elif options.which == "generate_cohort":
         generate_cohort()
+    elif options.which == "notebook":
+        if not options.skip_build:
+            docker_build(notebook_tag)
+        container_id = docker_run(notebook_tag, detach=True)
+        port = docker_port(container_id)
+        await_jupyter_http(port)
+        webbrowser.open(f"http://localhost:{port}", new=2)  # Open in a new tab
+        print(
+            "To stop this docker container, use Ctrl+ C, or the File -> Shut Down menu in Jupyter Lab"
+        )
+        stream_subprocess_output(["docker", "logs", "--follow", container_id])
 
 
 if __name__ == "__main__":
