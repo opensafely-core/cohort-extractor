d900218ba01315d780c70da4548a72150a8c4b54
Seb Bacon <seb.bacon@gmail.com>
2020-04-10 10:58:15 +0100

    Ensure things work in Windows

--- run.py
+++ run.py
@@ -29,14 +29,14 @@ target_dir = "/home/app/notebook"
 
 
 def relative_dir():
-    if sys.executable.endswith(".exe"):
+    if sys.executable == "run.exe":
         # This is a pyinstaller package on Windows; the `cwd` is
         # likely to be anything (e.g. the Github desktop AppData
         # space); `__file__` is some kind of temporary location
         # resulting from an internal unzip.
         relative_dir = os.path.dirname(os.path.realpath(sys.executable))
     else:
-        relative_dir = os.path.dirname(os.path.realpath(__file__))
+        relative_dir = os.getcwd()
     return relative_dir
 
 
@@ -263,6 +263,7 @@ def main():
         else:
             print(run_model("analysis", options.stata_path))
     elif options.which == "generate_cohort":
+        os.environ["DATABASE_URL"] = options.database_url
         generate_cohort()
     elif options.which == "notebook":
         if not options.skip_build:
