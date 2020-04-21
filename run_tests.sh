cce07a4c44477cbfaec6aa43bad157168af42ca2
David Evans <dave@evans.io>
2020-04-21 11:18:49 +0100

    Hopefully minimal changes to support running tests locally

---
+++ run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+exec docker-compose run \
+  --rm \
+  -e DATABASE_URL=mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server \
+  app \
+    py.test tests/
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,6 @@ def pytest_generate_tests(metafunc):
     """
     for line in open(Path(__file__).absolute().parent.parent / ".env-test"):
         k, v = line.split("=", 1)
-        os.environ[k.strip()] = v.strip()
+        k = k.strip()
+        if not os.environ.get(k):
+            os.environ[k] = v.strip()
