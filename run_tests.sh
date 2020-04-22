745a37cf98dcd593101f950fbc11bb423598588f
David Evans <dave@evans.io>
2020-04-22 16:58:13 +0100

    Support passing arguments to pytest

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ exec docker-compose run \
   --rm \
   -e DATABASE_URL=mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server \
   app \
-    py.test tests/
+    py.test "$@" tests/
