d22d7d72e15fb9eee79801931936b8acdeb9c580
Seb Bacon <seb.bacon@gmail.com>
2020-05-10 20:00:25 +0100

    Try hunting the file that's been generated

--- run_pyinstaller.py
+++ run_pyinstaller.py
@@ -1,6 +1,7 @@
 import subprocess
 import itertools
 import pkg_resources
+import os
 
 packages = list(pkg_resources.working_set)
 
@@ -10,4 +11,16 @@ args = list(
     )
 )
 cmd = ["pyinstaller"] + args + ["--onefile", "wrapper.py"]
-subprocess.run(cmd)
+subprocess.run(cmd, check=True)
+
+
+def find_all(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if name in f:
+                result.append(os.path.join(root, name))
+    return result
+
+
+print(find_all(".exe", "."))
