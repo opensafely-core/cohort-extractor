f1dfdf1cf4b7d6656eac8e1b79c7e7560f7d3002
Seb Bacon <seb.bacon@gmail.com>
2020-05-10 10:28:43 +0100

    Try building arguments in python

---
+++ run_pyinstaller.py
@@ -0,0 +1,13 @@
+import subprocess
+import itertools
+import pkg_resources
+
+packages = list(pkg_resources.working_set)
+
+args = list(
+    itertools.chain(
+        *zip(["--hidden-import"] * len(packages), [p.project_name for p in packages])
+    )
+)
+cmd = ["pyinstaller"] + args + ["wrapper.py"]
+subprocess.run(cmd)
