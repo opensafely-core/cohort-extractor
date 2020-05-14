ee25c5e33a3a99080c535953055a386220c2c1a0
Seb Bacon <seb.bacon@gmail.com>
2020-05-14 11:25:51 +0100

    Let user know which study definition is being processed
    This can be useful when hunting down bugs

--- run.py
+++ run.py
@@ -251,6 +251,7 @@ def make_chart(name, series, dtype):
 
 def generate_cohort(expectations_population):
     for study_name, suffix in list_study_definitions():
+        print(f"Generating cohort for {study_name}...")
         _generate_cohort(study_name, suffix, expectations_population)
 
 
