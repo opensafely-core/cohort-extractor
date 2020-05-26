4cc22f232bd02bb5a8d24e5cfeeabd210db613ba
Seb Bacon <seb.bacon@gmail.com>
2020-05-26 18:36:26 +0100

    Add setuptools support

---
+++ setup.py
@@ -0,0 +1,24 @@
+import runner
+
+from setuptools import setup, find_packages
+
+runner.__version__
+
+setup(
+    name="opensafely-cohort-extractor",
+    version=runner.__version__,
+    packages=find_packages(),
+    url="https://github.com/opensafely/cohort-extractor",
+    author="OpenSAFELY",
+    author_email="tech@opensafely.org",
+    install_requires=[
+        "pandas",
+        "pyodbc",
+        "pyyaml",
+        "requests",
+        "seaborn",
+        "sqlalchemy",
+        "sqlparse",
+    ],
+    scripts=["run.py"],
+)
