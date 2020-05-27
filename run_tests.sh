568437887b8a032c86b90e77ee5cf98d437e6938
Peter Inglesby <peter.inglesby@gmail.com>
2020-05-27 14:08:02 +0100

    Fix run_tests.sh
    * Fix quoting
    * Add entrypoint

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 exec docker-compose run \
   --rm \
-  -e DATABASE_URL=mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server \
+  --entrypoint /bin/bash \
+  -e DATABASE_URL='mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server' \
   app \
-    bash --login -- py.test "$@" tests/
+    --login -- py.test "$@" tests/
