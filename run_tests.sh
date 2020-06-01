b1148618c83c8bb44b2a4b845c3ee0c301402333
Peter Inglesby <peter.inglesby@gmail.com>
2020-06-01 14:04:47 +0100

    Add PRESTO_URL to run_tests.sh environment

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,6 @@ exec docker-compose run \
   --rm \
   --entrypoint /bin/bash \
   -e SQL_SERVER_URL='mssql+pyodbc://SA:Your_password123!@sql_server:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server' \
+  -e PRESTO_URL='presto://presto' \
   app \
     --login -- py.test "$@" tests/
