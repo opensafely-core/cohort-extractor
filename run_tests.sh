1b7f6d80552428374fa9a746c0b3332e45986eca
David Evans <dave@evans.io>
2020-06-01 16:52:51 +0100

    Fix test runner to work with new Dockerfile

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,8 @@
 exec docker-compose run \
   --rm \
   --entrypoint /bin/bash \
+  --volume "$PWD:/workspace" \
+  -e PYENV_VERSION=3.8.3 \
   -e DATABASE_URL='mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server' \
   app \
     --login -- py.test "$@"
