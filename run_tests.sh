d78f6fa9ee18d4f0e6edd62bdd73d5572e96d1e2
David Evans <dave@evans.io>
2020-06-01 15:41:28 +0100

    Support multiple backend configs in test environment

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,6 @@ exec docker-compose run \
   --entrypoint /bin/bash \
   --volume "$PWD:/workspace" \
   -e PYENV_VERSION=3.8.3 \
-  -e DATABASE_URL='mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server' \
+  -e TPP_DATABASE_URL='mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server' \
   app \
     --login -- py.test "$@"
