e040032d4f9e7d65e33cf7e0f906a51d3520502b
David Evans <dave@evans.io>
2020-06-03 12:10:34 +0100

    Add support for backend that uses Presto

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,7 @@ exec docker-compose run \
   --volume "$PWD:/workspace" \
   -e PYENV_VERSION=3.8.3 \
   -e TPP_DATABASE_URL='mssql+pyodbc://SA:Your_password123!@sql:1433/Test_OpenCorona?driver=ODBC+Driver+17+for+SQL+Server' \
+  -e ACME_DATABASE_URL=presto://presto:8080/sqlserver/dbo \
+  -e ACME_DATASOURCE_DATABASE_URL='mssql+pyodbc://SA:Your_password123!@sql:1433/Test_ACME?driver=ODBC+Driver+17+for+SQL+Server' \
   app \
     --login -- py.test "$@"
