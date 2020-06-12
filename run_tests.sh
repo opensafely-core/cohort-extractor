f37ff87c4070101996c613c64eecdf3850308e59
Peter Inglesby <peter.inglesby@gmail.com>
2020-06-12 13:21:25 +0100

    Rename "sql" container to "mssql"
    We use this over "sql_server" as that seems to be the more common way to
    refer to it in code (Presto's connector notwithstanding).

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,8 @@ exec docker-compose run \
   --entrypoint /bin/bash \
   --volume "$PWD:/workspace" \
   -e PYENV_VERSION=3.8.3 \
-  -e TPP_DATABASE_URL='mssql://SA:Your_password123!@sql:1433/Test_OpenCorona' \
-  -e ACME_DATABASE_URL=presto://presto:8080/sqlserver/dbo \
-  -e ACME_DATASOURCE_DATABASE_URL='mssql://SA:Your_password123!@sql:1433/Test_ACME' \
+  -e TPP_DATABASE_URL='mssql://SA:Your_password123!@mssql:1433/Test_OpenCorona' \
+  -e ACME_DATABASE_URL=presto://presto:8080/mssql/dbo \
+  -e ACME_DATASOURCE_DATABASE_URL='mssql://SA:Your_password123!@mssql:1433/Test_ACME' \
   app \
     --login -- py.test "$@"
