f6efeac36adc7457041711510975161d8ba45aa3
David Evans <dave@evans.io>
2020-08-13 12:31:48 +0100

    Add basic smoketest for measure generation

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,4 +1,10 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv
+from cohortextractor import (
+    StudyDefinition,
+    Measure,
+    patients,
+    codelist,
+    codelist_from_csv,
+)
 
 chronic_cardiac_disease_codes = codelist_from_csv(
     "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID"
@@ -15,19 +21,20 @@ systolic_blood_pressure_codes = codelist(["2469."], system="ctv3")
 diastolic_blood_pressure_codes = codelist(["246A."], system="ctv3")
 
 study = StudyDefinition(
+    index_date="2020-02-01",
     # Configure the expectations framework
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": "1900-01-01", "latest": "index_date"},
         "rate": "exponential_increase",
     },
     # This line defines the study population
     population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+        "index_date - 1 year", "index_date"
     ),
     # The rest of the lines define the covariates with associated GitHub issues
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/33
     age=patients.age_as_of(
-        "2020-02-01",
+        "index_date",
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
@@ -56,12 +63,20 @@ study = StudyDefinition(
         include_month=True,
         return_expectations={
             "incidence": 0.2,
-            "date": {"earliest": "1950-01-01", "latest": "today"},
+            "date": {"earliest": "1950-01-01", "latest": "index_date"},
+        },
+    ),
+    has_chronic_liver_disease=patients.with_these_clinical_events(
+        chronic_liver_disease_codes,
+        returning="binary_flag",
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "index_date"},
         },
     ),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/10
     bmi=patients.most_recent_bmi(
-        on_or_after="2010-02-01",
+        on_or_after="index_date",
         minimum_age_at_measurement=16,
         include_measurement_date=True,
         include_month=True,
@@ -74,7 +89,7 @@ study = StudyDefinition(
     bp_sys=patients.mean_recorded_value(
         systolic_blood_pressure_codes,
         on_most_recent_day_of_measurement=True,
-        on_or_before="2020-02-01",
+        on_or_before="index_date",
         include_measurement_date=True,
         include_month=True,
         return_expectations={
@@ -85,7 +100,7 @@ study = StudyDefinition(
     bp_dias=patients.mean_recorded_value(
         diastolic_blood_pressure_codes,
         on_most_recent_day_of_measurement=True,
-        on_or_before="2020-02-01",
+        on_or_before="index_date",
         include_measurement_date=True,
         include_month=True,
         return_expectations={
@@ -95,7 +110,7 @@ study = StudyDefinition(
     ),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/54
     stp=patients.registered_practice_as_of(
-        "2020-02-01",
+        "index_date",
         returning="stp_code",
         return_expectations={
             "rate": "universal",
@@ -103,7 +118,7 @@ study = StudyDefinition(
         },
     ),
     msoa=patients.registered_practice_as_of(
-        "2020-02-01",
+        "index_date",
         returning="msoa_code",
         return_expectations={
             "rate": "universal",
@@ -112,7 +127,7 @@ study = StudyDefinition(
     ),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/52
     imd=patients.address_as_of(
-        "2020-02-01",
+        "index_date",
         returning="index_of_multiple_deprivation",
         round_to_nearest=100,
         return_expectations={
@@ -121,7 +136,7 @@ study = StudyDefinition(
         },
     ),
     rural_urban=patients.address_as_of(
-        "2020-02-01",
+        "index_date",
         returning="rural_urban_classification",
         return_expectations={
             "rate": "universal",
@@ -130,7 +145,7 @@ study = StudyDefinition(
     ),
     recent_salbutamol_count=patients.with_these_medications(
         salbutamol_codes,
-        between=["2018-02-01", "2020-02-01"],
+        between=["index_date - 2 years", "index_date"],
         returning="number_of_matches_in_period",
         return_expectations={
             "incidence": 0.6,
@@ -138,3 +153,12 @@ study = StudyDefinition(
         },
     ),
 )
+
+measures = [
+    Measure(
+        id="liver_disease_by_stp",
+        numerator="has_chronic_liver_disease",
+        denominator="population",
+        group_by="stp",
+    ),
+]
---
+++ tests/test_smoketest.py
@@ -0,0 +1,40 @@
+"""
+Basic smoketest to check that invoking that the CLI entrypoint is working.
+This relies on the contents of `analyis/study_definition.py` being as expected.
+"""
+import csv
+import subprocess
+import sys
+
+
+def test_smoketest(tmp_path):
+    _cohortextractor(
+        "generate_cohort",
+        "--expectations-population",
+        "100",
+        "--index-date-range",
+        "2020-01-01 to 2020-04-01 by month",
+        "--output-dir",
+        tmp_path,
+    )
+    _cohortextractor(
+        "generate_measures", "--output-dir", tmp_path,
+    )
+    with open(tmp_path / "measure_liver_disease_by_stp.csv") as f:
+        contents = list(csv.reader(f))
+    assert contents[0] == [
+        "stp",
+        "has_chronic_liver_disease",
+        "population",
+        "value",
+        "date",
+    ]
+    assert len(contents) == 1 + (2 * 4)  # Header row + 2 STPs * 4 dates
+    dates = set(row[4] for row in contents[1:])
+    assert dates == {"2020-01-01", "2020-02-01", "2020-03-01", "2020-04-01"}
+
+
+def _cohortextractor(*args):
+    subprocess.run(
+        [sys.executable, "-m", "cohortextractor.cohortextractor", *args], check=True
+    )
