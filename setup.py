54de846f91c1f6e6115a17f6e8bd5e07204af8a5
Seb Bacon <seb.bacon@gmail.com>
2020-11-05 11:57:30 +0000

    fix: upgrade jobrunner
    This contains further cross-platform (path-related) fixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="tech@opensafely.org",
     python_requires=">=3.7",
     install_requires=[
-        "opensafely-jobrunner>=1.7.1",
+        "opensafely-jobrunner>=1.7.2",
         "pandas",
         "prettytable",
         "pyyaml",
