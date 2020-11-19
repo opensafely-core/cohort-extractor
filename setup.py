718861edd643b9d885b7b15dd548e583208ff768
Seb Bacon <seb.bacon@gmail.com>
2020-11-19 10:14:58 +0000

    fix: bump jobrunner

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email="tech@opensafely.org",
     python_requires=">=3.7",
     install_requires=[
-        "opensafely-jobrunner>=1.7.2",
+        "opensafely-jobrunner>=1.8.1",
         "pandas",
         "prettytable",
         "pyyaml",
