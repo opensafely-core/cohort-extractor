be48fb72ceda9d219b75831ce1275f4ae0db6b6e
David Evans <dave.evans@thedatalab.org>
2020-12-08 12:48:36 +0000

    Add doc string in (hopefully) correct format for mkdocs

--- cohortextractor/measure.py
+++ cohortextractor/measure.py
@@ -1,5 +1,31 @@
 class Measure:
     def __init__(self, id, denominator, numerator, group_by=None):
+        """
+        Creates a "measure" using data extracted by the StudyDefinition defined
+        in the same file.
+
+        Args:
+            id: A string used for the output filename (which will be
+                `measure_<id>.csv`).  Only alphanumeric and underscore characters
+                allowed.
+            numerator: A column name from the study definition. This must be
+                numeric or boolean. For boolean columns the value of the numerator
+                will be the number of patients with the value "true".
+            denominator: A column name from the study definition, or the
+                special name "population". Columns must be numeric or boolean.
+                For boolean columns the value of the denominator will be the
+                number of patients with the value "true". Using the
+                "population" denominator will give you the total number of
+                patients in the cohort.
+            group_by: A column name, or a list of column names, from the study
+                definition to group results by. Use the special column
+                "population" to treat the entire population as a single group.
+                Set group_by to None (or omit it entirely) to perform no
+                grouping and leave the data at individual patient level.
+
+        Returns:
+            Measure instance
+        """
         self.id = id
         self.denominator = denominator
         self.numerator = numerator
