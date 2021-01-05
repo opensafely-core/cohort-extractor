47732eb0590a5bddcfaaf18072252e5cfde85f09
Peter Inglesby <peter.inglesby@gmail.com>
2021-01-05 10:03:03 +0000

    Add shebang and set -u to get_certs.sh

--- certs/providerplus.emishealthinsights.co.uk/get_certs.sh
+++ certs/providerplus.emishealthinsights.co.uk/get_certs.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env sh
+
+set -u
+
 openssl s_client -showcerts -connect providerplus.emishealthinsights.co.uk:443 2>/dev/null </dev/null | \
         sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | \
         awk 'split_after == 1 {n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > n+1 ".crt"}'
