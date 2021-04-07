85da5d109b543b474efc16ffb09975269a8779e9
Ben Butler-Cole <ben@bridesmere.com>
2021-04-07 20:59:12 +0100

    fix: Make docstring consistent with argument order in code

--- cohortextractor/measure.py
+++ cohortextractor/measure.py
@@ -8,15 +8,15 @@ class Measure:
             id: A string used for the output filename (which will be
                 `measure_<id>.csv`).  Only alphanumeric and underscore characters
                 allowed.
-            numerator: A column name from the study definition. This must be
-                numeric or boolean. For boolean columns the value of the numerator
-                will be the number of patients with the value "true".
             denominator: A column name from the study definition, or the
                 special name "population". Columns must be numeric or boolean.
                 For boolean columns the value of the denominator will be the
                 number of patients with the value "true". Using the
                 "population" denominator will give you the total number of
                 patients in the cohort.
+            numerator: A column name from the study definition. This must be
+                numeric or boolean. For boolean columns the value of the numerator
+                will be the number of patients with the value "true".
             group_by: A column name, or a list of column names, from the study
                 definition to group results by. Use the special column
                 "population" to treat the entire population as a single group.
