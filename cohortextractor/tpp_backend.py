d5ba7ce63c719a49caf557fba0ef9ff11b250b7f
Tom Ward <tomward@fmail.co.uk>
2021-07-28 14:56:25 +0100

    docs: remove out of date comment

--- cohortextractor/tpp_backend.py
+++ cohortextractor/tpp_backend.py
@@ -2360,7 +2360,7 @@ class TPPBackend:
         # with_these_treatment_function_codes=tfc_codelist,
         # with_these_procedure_codes=OPCS_codelist,
         # referral_request_received_date="YYYY-MM-DD",
-        between=None, # maybe this should be `between` (but how does that work?)
+        between=None,
         returning="binary_flag"
     ):
         date_condition, date_joins = self.get_date_condition(
