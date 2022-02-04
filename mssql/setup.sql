28e638579159cb66f025b3f8d56476863ea2df75
Becky Smith <becky.smith@thedatalab.org>
2022-02-04 14:53:51 +0000

    Add comment for sql server compatility level

--- mssql/setup.sql
+++ mssql/setup.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE Test_OpenCorona;
 ALTER DATABASE Test_OpenCorona SET COMPATIBILITY_LEVEL = 100;
 CREATE DATABASE OPENCoronaTempTables;
+-- Set compatibility level to match the TPP database (uses SQL Server 2016, but with
+-- compatibility set to 100 - equivalent to SQL Server 2008)
 ALTER DATABASE OPENCoronaTempTables SET COMPATIBILITY_LEVEL = 100;
 CREATE DATABASE Test_EMIS;
 
