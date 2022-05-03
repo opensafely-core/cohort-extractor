73ceaa93203ed4d02d8b426764788120c24519c9
Peter Inglesby <peter.inglesby@gmail.com>
2022-05-03 16:08:23 +0100

    Shuffle docstrings

--- cohortextractor/generate_codelist_report.py
+++ cohortextractor/generate_codelist_report.py
@@ -11,17 +11,6 @@ logger = structlog.get_logger()
 
 
 def generate_codelist_report(output_dir, codelist_path, start_date, end_date):
-    """
-    Generate a pair of CSV files reporting usage of codes in given codelist.
-
-        * counts_per_code counts how many times each code appears in the given timeframe
-        * counts_per_week counts how many times all the codes appear in each week in the
-            given timeframe
-
-    Weeks begin on a Monday, and a code appearing on (say) a Wednesday is counted as
-    belonging to the following Monday.
-    """
-
     os.makedirs(output_dir, exist_ok=True)
     connection = mssql_dbapi_connection_from_url(os.environ["DATABASE_URL"])
     cursor = connection.cursor()
@@ -34,6 +23,16 @@ def generate_codelist_report(output_dir, codelist_path, start_date, end_date):
 
 
 def generate_counts(cursor, output_dir, codelist_path, start_date, end_date):
+    """Generate a pair of CSV files reporting usage of codes in given codelist.
+
+        * counts_per_code counts how many times each code appears in the given timeframe
+        * counts_per_week counts how many times all the codes appear in each week in the
+            given timeframe
+
+    Weeks begin on a Monday, and a code appearing on (say) a Wednesday is counted as
+    belonging to the following Monday.
+    """
+
     codelist = codelist_from_csv(codelist_path, "snomedct")
 
     queries = codelist_queries(codelist) + [
@@ -65,7 +64,8 @@ def generate_counts(cursor, output_dir, codelist_path, start_date, end_date):
 
 
 def generate_list_sizes(cursor, output_dir, end_date):
-    """Find the number of patients registered at each practice on the end_date.
+    """Generate a CSV file reporting the number of patients registered at each practice
+    on the end_date.
 
     This is to be used as the denominator for deciles charts.  It will lead to some odd
     rates for practices where the population has changed a lot, but is acceptable while
