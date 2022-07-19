548386f1e7947328529d665b97adc14852936b27
Peter Inglesby <peter.inglesby@gmail.com>
2022-07-19 08:49:43 +0100

    fix: Add warning to with_gp_consultations docs
    This moves the warning from the bottom of the section to the top.
    
    Addresses part of opensafely-core/opensafely-cli#120

--- cohortextractor/patients.py
+++ cohortextractor/patients.py
@@ -1804,6 +1804,15 @@ def with_gp_consultations(
     return_expectations=None,
 ):
     """
+    !!! warning
+    A "consultation" as returned by this function may not actually be a consultation between a health care professional and patient.
+
+    For example, simple administrative tasks such as updating a patient's details may also cause creation of a "consultation" record.
+
+    It is, therefore, extremely unlikely you can use this field to infer anything about real clinician-patient interactions,
+    and it is even more difficult to compare activity between practices as recording behaviour can vary.
+    Some EHRs even delete consultation data for a patient when they switch practice.
+
     These are GP-patient interactions, either in person or via phone/video
     call. The concept of a "consultation" in EHR systems is generally broader
     and might include things like updating a phone number with the
