363ad18bd7c5ea83febfd7d2eaf54c94592c5320
Iain Dillingham <iain@dillingham.me.uk>
2022-10-25 11:43:32 +0100

    Fix warning callout
    The text hasn't changed; it's simply indented by four spaces such that
    MkDocs formats it as a callout.

--- cohortextractor/patients.py
+++ cohortextractor/patients.py
@@ -1825,13 +1825,13 @@ def with_gp_consultations(
 ):
     """
     !!! warning
-    A "consultation" as returned by this function may not actually be a consultation between a health care professional and patient.
+        A "consultation" as returned by this function may not actually be a consultation between a health care professional and patient.
 
-    For example, simple administrative tasks such as updating a patient's details may also cause creation of a "consultation" record.
+        For example, simple administrative tasks such as updating a patient's details may also cause creation of a "consultation" record.
 
-    It is, therefore, extremely unlikely you can use this field to infer anything about real clinician-patient interactions,
-    and it is even more difficult to compare activity between practices as recording behaviour can vary.
-    Some EHRs even delete consultation data for a patient when they switch practice.
+        It is, therefore, extremely unlikely you can use this field to infer anything about real clinician-patient interactions,
+        and it is even more difficult to compare activity between practices as recording behaviour can vary.
+        Some EHRs even delete consultation data for a patient when they switch practice.
 
     These are GP-patient interactions, either in person or via phone/video
     call. The concept of a "consultation" in EHR systems is generally broader
